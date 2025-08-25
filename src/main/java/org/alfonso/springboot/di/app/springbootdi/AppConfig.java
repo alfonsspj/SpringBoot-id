85a0bd512414d7483e135ab33d3d774847400f8b
alfonsspj <alfonsspj@gmail.com>
2025-08-24 22:06:21 -0300

    Variantes en el componente registrado con @Configuration y @Bean

--- src/main/java/org/alfonso/springboot/di/app/springbootdi/AppConfig.java
+++ src/main/java/org/alfonso/springboot/di/app/springbootdi/AppConfig.java
@@ -2,15 +2,21 @@ package org.alfonso.springboot.di.app.springbootdi;
 
 import org.alfonso.springboot.di.app.springbootdi.repositories.ProductRepository;
 import org.alfonso.springboot.di.app.springbootdi.repositories.ProductRepositoryJson;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.io.Resource;
 
 
 @Configuration
 @PropertySource("classpath:config.properties")
 public class AppConfig {
+
+    @Value("classpath:json/product.json")
+    private Resource resource;
+
 
 //    ProductRepository tipo generico
 //    public ProductRepository productRepositoryJson(){
@@ -19,7 +25,9 @@ public class AppConfig {
 //    @Bean // cuando el componente es anotado con Bean podemos omitir el public
     @Bean("productJson") // personalizado con nombre distinto al del metodo -- nombre logico que va a tener el componente
 //    @Primary
+//    ProductRepository productRepositoryJson(){
     ProductRepository productRepositoryJson(){
-        return new ProductRepositoryJson();
+        return new ProductRepositoryJson(resource);
+//        return new ProductRepositoryJson();// lo obtiene mediante classPath de forma programatica
     }
 }
--- src/main/java/org/alfonso/springboot/di/app/springbootdi/repositories/ProductRepositoryJson.java
+++ src/main/java/org/alfonso/springboot/di/app/springbootdi/repositories/ProductRepositoryJson.java
@@ -2,29 +2,53 @@ package org.alfonso.springboot.di.app.springbootdi.repositories;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.alfonso.springboot.di.app.springbootdi.models.Product;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
-
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
-
+// clase comun y corriente
 public class ProductRepositoryJson implements ProductRepository{
 
     private List<Product> list;
 
+    // forma declarativa
+    /*@Value("classpath:json/product.json")
+    private Resource resource;*/ // devuelve null ya que value no funciona en un contexto que no este dentro de spring, que no sea un componente spring
+
     public ProductRepositoryJson() {
+
+        // forma programatica
 //        ClassPathResource resource = new ClassPathResource("json/products.json");// classPathResource hereda de Resource
         Resource resource = new ClassPathResource("json/product.json");//
-        // convierte un archivo o inputStream a un objeto de java
+        readValueJson(resource);
+
+        /*// convierte un archivo o inputStream a un objeto de java
         ObjectMapper objectMapper = new ObjectMapper();
         try {
-            list = Arrays.asList(objectMapper.readValue(resource.getFile(), Product[].class)); // toma como argumento un arreglo de objetos y ese objeto va a ser el resultado de parsear
+//            list = Arrays.asList(objectMapper.readValue(resource.getFile(), Product[].class)); // toma como argumento un arreglo de objetos y ese objeto va a ser el resultado de parsear
+            list = Arrays.asList(objectMapper.readValue(resource.getInputStream(), Product[].class)); //
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }*/
+    }
+
+    public ProductRepositoryJson(Resource resource) {
+        readValueJson(resource);
+    }
+
+    private void readValueJson(Resource resource) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            list = Arrays.asList(objectMapper.readValue(resource.getInputStream(), Product[].class)); //
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
+
+
 
     @Override
     public List<Product> findAll() {
